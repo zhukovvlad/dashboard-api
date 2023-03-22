e55f92de83ac25317d3c18504440a6ceb6e77f74
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-03-22 23:27:55 +0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response, NextFunction } from "express";
+import { userRouter } from "./users/users.js";
+
+const port = 8000;
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Time is ", Date.now());
+  next();
+});
+
+app.get("/hello", (req, res) => {
+  throw new Error("Error!");
+});
+
+app.use("/users", userRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(err.message);
+  res.status(401).send(err.message);
+});
+
+app.listen(port, () => {
+  console.log(`Server started at https://localhost:${port}`);
+});
